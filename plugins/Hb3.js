ee6914cae75270ce27606e6a8531efd921ecde70
Ayushdeveloper001 <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-01-09 21:10:18 +0530

    Create Hb3.js

---
+++ plugins/Hb3.js
@@ -0,0 +1,11 @@
+================================================
+*@video: Send Video
+===============================================
+*//
+cmd({hb}
+  pattern: 'video',
+  desc: 'Sends video',
+  category: 'gen',
+  use: '<option>',
+}, async(Void,citel,text) => {Void}
+await Void.sendMessage(citel.chat,{image:{url: 'https://imgur.com/gallery/tjWjr2x'}, caption: "HAPPY BIRTHDAY" })
